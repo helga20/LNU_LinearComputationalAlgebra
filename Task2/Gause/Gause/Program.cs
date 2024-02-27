0d89171a0528683238ac1297527751a12f7dc4a9
Olia Kravets <94845931+helga20@users.noreply.github.com>
2024-02-27 19:27:39 +0200

    Add files via upload

---
+++ Task2/Gause/Gause/Program.cs
@@ -0,0 +1,100 @@
+﻿using System.Drawing;
+
+double[,] A = new double[,] { { 1.0, 5, 3, -4, 20 }, { 3, 1, -2, 0, 9 }, { 5, -7, 0, 10, -9 }, { 0, 3, -5, 0, 1 } };
+
+var max = Math.Abs(A[0, 0]);
+int maxLine = 0;
+int maxColumn = 0;
+int rows = A.GetLength(0);
+int columns = A.GetLength(1);
+
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns - 1; j++)
+    {
+        if (Math.Abs(A[i, j]) > max)
+        {
+            max = Math.Abs(A[i, j]);
+            maxLine = i;
+            maxColumn = j;
+        }
+    }
+}
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        if (i != maxLine)
+        {
+            A[i, j] = A[i, j] + A[maxLine, j] * (-A[i, maxColumn] / A[maxLine, maxColumn]);
+        }
+    }
+}
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.Write(A[i, j] + "  ");
+    }
+    Console.WriteLine();
+}
+
+
+
+
+double[] X = new double[A.GetLength(0)];
+int Size = A.GetLength(1);
+for (int j = 0; j < Size - 1; j++)
+{
+    var max = Math.Abs(A[j, j]);
+    int maxLine = j;
+    for (int i = j + 1; i < Size; i++)
+    {
+        if (Math.Abs(A[i, j]) > max)
+        {
+            max = Math.Abs(A[i, j]);
+            maxLine = i;
+        }
+    }
+
+    if (maxLine != j)
+    {
+        for (int k = j; k < Size; k++)
+        {
+            (A[j, k], A[maxLine, k]) = (A[maxLine, k], A[j, k]);
+        }
+        (B[j], B[maxLine]) = (B[maxLine], B[j]);
+    }
+
+    for (int i = j + 1; i < Size; i++)
+    {
+        double mult = A[i, j] / max;
+        for (int k = j; k < Size; k++)
+        {
+            A[i, k] -= mult * A[j, k];
+        }
+        B[i] -= mult * B[j];
+    }
+}
+for (int j = Size - 1; j >= 0; j--)
+{
+    if (A[j, j] == 0)
+    {
+        Console.WriteLine("Wrong input");
+        return;
+    }
+    X[j] = B[j] / A[j, j];
+    for (int i = 0; i < j; i++)
+    {
+        B[i] -= A[i, j] * X[j];
+    }
+}
+
+/*Console.WriteLine("Result = : ");
+foreach (var x in X)
+{
+    Console.Write(x + "  ");
+}*/
---
+++ task4/task4/Program.cs
@@ -0,0 +1,180 @@
+﻿using System;
+
+
+////double[,] A = new double[,]{
+////    {1, 2, 3, 4, 0, 0 },
+////    { 2, 5, 8, 7, 0, 0},
+////    { 3, 8, 14, 12, 3, 4},
+////    { 4, 7, 12, 22, 8, 7},
+////    { 0, 0, 3, 8, 14, 12},
+////    { 0, 0, 4, 7, 12, 22 } };
+
+////double[] b = new double[] { 10, 22, 44, 60, 37, 45 };
+
+//double[,] A = {
+//    {1, 2, 3, 4},
+//    {2, 5, 8, 7},
+//    {3, 8, 15, 14},
+//    {4, 7, 14, 27}
+//};
+
+//double[] b = { 10, 22, 40, 52 };
+//double k = -0.4285714285714288 + 2* -0.4285714285714288 + 3 * -0.4285714285714288+4* -0.4285714285714288;
+//Console.WriteLine(k);
+
+//BlockGaussSolver.Solve(A, b, 2);
+////double[] x = solver.Solve(A, b, 2);
+
+//Console.WriteLine("Solution:");
+//for (int i = 0; i < b.Length; i++)
+//{
+//    Console.WriteLine("x[{0}] = {1}", i, b[i]);
+//}
+
+
+
+//public class FrontalSolver
+//{
+//    public double[] Solve(double[,] A, double[] b)
+//    {
+//        int n = A.GetLength(0);
+//        Factorize(A);
+//        ForwardSubstitution(A, b);
+
+//        // Backward substitution
+//        double[] x = new double[n];
+//        for (int i = n - 1; i >= 0; i--)
+//        {
+//            x[i] = b[i];
+//            for (int j = i + 1; j < n; j++)
+//            {
+//                x[i] -= A[i, j] * x[j];
+//            }
+//            x[i] /= A[i, i];
+//        }
+
+//        return x;
+//    }
+
+//    private void Factorize(double[,] A)
+//    {
+//        int n = A.GetLength(0);
+//        for (int j = 0; j < n; j++)
+//        {
+//            // Find block rows
+//            int[] rows = new int[n];
+//            int k = 0;
+//            for (int i = j; i < n; i++)
+//            {
+//                if (A[i, j] != 0)
+//                {
+//                    rows[k] = i;
+//                    k++;
+//                }
+//            }
+
+//            // Build block matrix
+//            double[,] B = new double[k, k];
+//            for (int i = 0; i < k; i++)
+//            {
+//                for (int l = 0; l < k; l++)
+//                {
+//                    B[i, l] = A[rows[i], rows[l]];
+//                }
+//            }
+
+//            // Factorize block matrix
+//            LUFactorization(B);
+
+//            // Update A
+//            for (int i = j; i < n; i++)
+//            {
+//                if (A[i, j] != 0)
+//                {
+//                    for (int l = 0; l < k; l++)
+//                    {
+//                        A[i, rows[l]] = B[(i - j), l];
+//                    }
+//                }
+//            }
+
+//            j += k - 1;
+//        }
+//    }
+
+//    private void LUFactorization(double[,] A)
+//    {
+//        int n = A.GetLength(0);
+//        for (int k = 0; k < n - 1; k++)
+//        {
+//            for (int i = k + 1; i < n; i++)
+//            {
+//                A[i, k] /= A[k, k];
+//                for (int j = k + 1; j < n; j++)
+//                {
+//                    A[i, j] -= A[i, k] * A[k, j];
+//                }
+//            }
+//        }
+//    }
+
+//    private void ForwardSubstitution(double[,] A, double[] b)
+//    {
+//        int n = A.GetLength(0);
+//        for (int k = 0; k < n - 1; k++)
+//        {
+//            for (int i = k + 1; i < n; i++)
+//            {
+//                b[i] -= A[i, k] * b[k];
+//            }
+//        }
+//    }
+//}
+
+
+
+
+public partial class Program
+{
+    public static void Main()
+    {
+        double[,] matrix = {
+            { 1, 2, 3, 4, 0, 0},
+            {2, 5, 8, 7, 0, 0 },
+            {3, 8, 14, 12, 3, 4 },
+            {4, 7, 12, 22, 8, 7 },
+            {0, 0, 3, 8, 14, 12 },
+            {0, 0, 4, 7, 12, 22}
+        };
+        
+        double[] b = { 10, 22, 44, 60, 37, 45 };
+    }
+    public void solve(double[,] A, double[] b)
+    {
+
+    }
+
+    public double[,] Multiply(double[,] a, double[,] b)
+    {
+        int n = a.GetLength(0);
+        int m = b.GetLength(1);
+        double[,] result_matrix = new double[n, m];
+
+        for (int i = 0; i < n; i++) 
+        {
+            for (int j = 0; j < m; j++)
+            {
+                result_matrix[i, j] = 0;
+
+                for (var k = 0; k < a.GetLength(1); k++)
+                {
+                    result_matrix[i, j] += a[i, k] * b[k, j];
+                }
+            }
+        }
+
+        return result_matrix;
+    }
+}
+     
+
